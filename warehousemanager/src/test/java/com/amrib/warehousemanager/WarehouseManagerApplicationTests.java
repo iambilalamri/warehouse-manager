34b63c507676a67b5ab5637ed2856018426abd2a
Bilal AMRI <iambilalamri@gmail.com>
2022-04-19 04:03:38 +0200

    [master]- Start Project

---
+++ warehousemanager/src/test/java/com/amrib/warehousemanager/WarehouseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrib.warehousemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
